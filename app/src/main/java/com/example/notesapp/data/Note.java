5a27f7be0485bc68d69eba2397ef4ace7ec71d56
Adith A Danthi <adith.danthi@gmail.com>
2019-10-05 20:23:55 +0530

    removed files

---
+++ app/src/main/java/com/example/notesapp/data/Note.java
@@ -0,0 +1,44 @@
+package com.example.notesapp.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int mNoteId;
+
+    @ColumnInfo(name = "title")
+    private String mNoteLabel;
+
+    @ColumnInfo(name = "content")
+    private String mNoteContent;
+
+
+    public Note (String noteLabel, String noteContent){
+        this.mNoteLabel = noteLabel;
+        this.mNoteContent = noteContent;
+    }
+
+    @Ignore
+    public Note (int noteId, String noteLabel, String noteContent){
+        this.mNoteLabel = noteLabel;
+        this.mNoteId = noteId;
+        this.mNoteContent = noteContent;
+    }
+
+    public String getLabel(){
+        return this.mNoteLabel;
+    }
+
+    public String getContent(){
+        return this.mNoteContent;
+    }
+
+    public int getNoteId(){
+        return this.mNoteId;
+    }
+
+}
---
+++ app/src/main/java/com/example/notesapp/database/NotesDatabase.java
@@ -0,0 +1,63 @@
+package com.example.notesapp.database;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.notesapp.data.Note;
+
+@Database(entities = {Note.class},version = 1, exportSchema = false)
+public abstract class NotesDatabase extends RoomDatabase {
+
+    public abstract NotesDao notesDao();
+
+    public static NotesDatabase INSTANCE;
+
+    public static NotesDatabase getDatabase(final Context context){
+        if(INSTANCE==null){
+            synchronized (NotesDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            NotesDatabase.class,
+                            "notes_database"
+                    )       .fallbackToDestructiveMigration()
+                            /*.addCallback(sRoomDatabaseCallback)*/
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    /*
+
+    If notes need to be added by default on creation of database
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase SQLdb){
+            super.onCreate(SQLdb);
+            new populateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class populateDbAsync extends AsyncTask<Void,Void,Void>{
+        private final NotesDao mDao;
+
+        populateDbAsync(NotesDatabase db){
+            mDao = db.notesDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+    }
+
+    */
+}
